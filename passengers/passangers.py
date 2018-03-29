6b4a27bb8a3a6efc5fc8c8990dd4e93989410293
Max Odinokiy <max.odinokiy@gmail.com>
2018-03-30 00:50:40 +0300

    changes

--- passengers/passangers.py
+++ passengers/passangers.py
@@ -6,42 +6,36 @@ def process(data, events, car):
     train_cars = {}
     cars = list()
 
-
     for train in data:
         for carr in train["cars"]:
             for man in carr["people"]:
                 pas_car[man] = carr["name"]
+
         train_cars[train["name"]] = [carr["name"] for carr in train["cars"]]
 
-
     for event in events:
-        if event["type"] == "walk":
-            for man in pas_car:
-                if man == event["passenger"]:
-                    for train in train_cars:
-                        for counter, carr in enumerate(train_cars[train]):
-                            if pas_car[man] == carr:
-                                if 0 <= counter + event["distance"] < len(train_cars[train]):
-                                    pas_car[man] = train_cars[train][counter + event["distance"]]
-                                    break
-                                else:
-                                    return -1
-        
-        else:
+        if event["type"] == "walk" and pas_car[event["passenger"]]:
             for train in train_cars:
-                if train == event["train_from"]:
-                    if 0 < event["cars"] <= len(train_cars[train]):
-                        lenght = len(train_cars[train]) - event["cars"]
-                        for number in range(event["cars"]):
-                            cars.append(train_cars[train][lenght + number])
-                        del train_cars[train][-event["cars"]:]
-                        for train2 in train_cars:
-                            if train2 == event["train_to"]:
-                                for number in range(event["cars"]):
-                                    train_cars[train2].append(cars[number])
-                        cars.clear()
-                    else:
-                        return -1
+                for counter, carr in enumerate(train_cars[train]):
+                    if pas_car[event["passenger"]] == carr:
+                        if 0 <= counter + event["distance"] < len(carr):
+                            pas_car[event["passenger"]] = train_cars[train][counter + event["distance"]]
+                            break
+                        else:
+                            return -1
+
+        elif train_cars[event["train_from"]]:
+            if 0 < event["cars"] <= len(train_cars[event["train_from"]]):
+                for number in range(event["cars"]):
+                    cars.append(train_cars[event["train_from"]][-event["cars"] + number])
+                del train_cars[event["train_from"]][-event["cars"]:]
+                for number in range(event["cars"]):
+                    train_cars[event["train_to"]].append(cars[number])
+                cars.clear()
+            else:
+                return -1
+        else:
+            return -1
 
     count = 0
     for man in pas_car:
